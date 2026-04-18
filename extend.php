c9877b6e282cf687a819c44977d593c1a66d45ea
Edge <dcc@monika.love>
2026-04-18 17:09:32 +0800

    ...

--- extend.php
+++ extend.php
@@ -18,8 +18,8 @@ use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
 
 return [
-    // (new Extend\Frontend('forum'))
-    //     ->js(__DIR__ . '/js/dist/forum.js'),
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__ . '/js/dist/forum.js'),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->MediaEmbed->add(
